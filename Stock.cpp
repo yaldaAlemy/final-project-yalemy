9aa9030cce8fd2722054f804e4ab7ec19267d313
Yalda Alemy <yalemy@mail.umw.edu>
2024-11-24 22:56:28 -0500

    added a few setter and getter methods to stock.cpp

---
+++ Stock.cpp
@@ -0,0 +1,34 @@
+#include "Stock.h"
+
+//constructor that will be edited later
+Stock::Stock() {}
+
+
+//Getter method
+
+double Stock::get_Price() const {
+
+    return 0.0;
+}
+
+//Getter method 
+
+int get_Quantity() const {
+
+    return 0;
+}
+
+
+//setter methods which has void return type 
+
+void Stock::set_Price(double p) {
+     price = p;
+}
+
+//for setting the quantity 
+
+void Stock::set_Quantity(int q) {
+    quantity = q;
+}
+
+
